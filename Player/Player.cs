2670ab53fff0e993c3b89c1c6be9bd7d3cf14f45
Braden Walters <vc@braden-walters.info>
2022-01-29 00:17:39 +0100

    Player movement and gravity.

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,14 +3,21 @@ using Input;
 
 namespace Player {
   public class Player : KinematicBody {
+    [Export] private float playerSpeed;
+
+    private float gravity;
+
+    public override void _Ready() {
+      this.gravity = -((float) ProjectSettings.GetSetting("physics/3d/default_gravity"));
+    }
+
     public override void _PhysicsProcess(float delta) {
       var movement = Godot.Input.GetVector(
         ActionTypes.MoveLeft,
         ActionTypes.MoveRight,
         ActionTypes.MoveDown,
         ActionTypes.MoveUp);
-      if (movement != Vector2.Zero)
-        GD.Print($"Current vector is {movement}");
+      MoveAndSlide(new Vector3(movement.x * this.playerSpeed, gravity, 0f), Vector3.Up);
     }
 
     public override void _Input(InputEvent @event) {

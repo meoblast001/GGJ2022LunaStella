94ac09b21438a14b67c793057d449236942df864
Braden Walters <vc@braden-walters.info>
2022-01-29 11:57:00 +0100

    Jumping.

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,11 +1,14 @@
+using System;
 using Godot;
 using Input;
 
 namespace Player {
   public class Player : KinematicBody {
     [Export] private float playerSpeed;
+    [Export] private float jumpSpeed;
 
     private float gravity;
+    private float yVelocity = 0;
 
     public override void _Ready() {
       this.gravity = -((float) ProjectSettings.GetSetting("physics/3d/default_gravity"));
@@ -17,14 +20,20 @@ namespace Player {
         ActionTypes.MoveRight,
         ActionTypes.MoveDown,
         ActionTypes.MoveUp);
-      MoveAndSlide(new Vector3(movement.x * this.playerSpeed, gravity, 0f), Vector3.Up);
+      yVelocity = Math.Max(yVelocity + delta * gravity, gravity);
+      MoveAndSlide(new Vector3(movement.x * this.playerSpeed, yVelocity, 0f), Vector3.Up);
     }
 
     public override void _Input(InputEvent @event) {
       if (@event.IsActionPressed(ActionTypes.Jump))
-        GD.Print("Jump pressed");
+        Jump();
       else if (@event.IsActionPressed(ActionTypes.Interact))
         GD.Print("Interact");
+    }
+
+    private void Jump() {
+      if (IsOnFloor())
+        this.yVelocity = this.jumpSpeed;
     }
   }
 }

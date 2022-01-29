94b8557c9e3292c5e58c6a31b7dd0ab244ba581e
Braden Walters <vc@braden-walters.info>
2022-01-29 19:17:12 +0100

    Fixed physics on door.

--- Door/Door.cs
+++ Door/Door.cs
@@ -1,22 +1,20 @@
 using Godot;
 
 namespace Door {
-  public class Door : KinematicBody {
-
-	private bool isOpen = false;
-	
-	private void _on_Switch_OpenDoor(bool isDoorOpen) {
-		if (isDoorOpen == true) {
-			this.isOpen = true;
-			//TranslateObjectLocal(Transform.basis.y * (this.transformFactor));
-			GetNode<AnimationPlayer>("AnimationPlayer").Play("Open");
-		} else {
-			this.isOpen = false;
-			//TranslateObjectLocal(-Transform.basis.y * (this.transformFactor));
-			GetNode<AnimationPlayer>("AnimationPlayer").Play("Close");
-		}
-	}
-
+  public class Door : Spatial {
+    private bool isOpen = false;
+    
+    private void _on_Switch_OpenDoor(bool isDoorOpen) {
+      if (isDoorOpen == true) {
+        this.isOpen = true;
+        //TranslateObjectLocal(Transform.basis.y * (this.transformFactor));
+        GetNode<AnimationPlayer>("AnimationPlayer").Play("Open");
+      } else {
+        this.isOpen = false;
+        //TranslateObjectLocal(-Transform.basis.y * (this.transformFactor));
+        GetNode<AnimationPlayer>("AnimationPlayer").Play("Close");
+      }
+    }
   }
 }
 
